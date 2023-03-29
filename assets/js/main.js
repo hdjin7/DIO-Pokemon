effed7b987c371797d04ac20cbd49f7ffd6c43d8
hdjin <hebertfbarbosa@hotmail.com>
2023-03-29 16:28:56 -0300

    Criando mecanismo para limitar na primeira geração de Pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,49 @@
-function convertPokemonToLi(pokemon) {
-  return `
-  <li class="pokemon">
-  <span class="number">#${pokemon.number}</span>
-  <span class="name">${pokemon.name}</span>
+const pokemonList = document.getElementById("pokemonList");
+const btnLoad = document.getElementById("btnLoad");
 
-  <div class="detail">
-      <ol class="types">
-      ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join("")}
-      </ol>
+const maxLimit = 151;
+const limit = 10;
+let offset = 0;
 
-      <img src="${pokemon.photo}"
-          alt="${pokemon.name}">
-  </div>
-</li>
-`;
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">#${pokemon.number}</span>
+      <span class="name">${pokemon.name}</span>
+    
+      <div class="detail">
+          <ol class="types">
+          ${pokemon.types
+            .map((type) => `<li class="type ${type}">${type}</li>`)
+            .join("")}
+          </ol>
+    
+          <img src="${pokemon.photo}"
+              alt="${pokemon.name}">
+      </div>
+    </li>
+    `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
 }
 
-const pokemonList = document.getElementById("pokemonList");
+loadPokemonItens(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  const newHtml = pokemons.map(convertPokemonToLi).join("");
-  pokemonList.innerHTML = newHtml;
+btnLoad.addEventListener("click", () => {
+  offset += limit;
+  const qtdNextPage = offset + limit;
+
+  if (qtdNextPage >= maxLimit) {
+    let newLimit = maxLimit - offset;
+    loadPokemonItens(offset, newLimit);
+
+    btnLoad.parentElement.removeChild(btnLoad);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
 });
